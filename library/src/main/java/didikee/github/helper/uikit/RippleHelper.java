1475c2fb43068321809a27a65cfeedcdb082c81c
didikee <didikee@qq.com>
2018-12-11 13:26:26 +0800

    urifixutil 更新

---
+++ library/src/main/java/didikee/github/helper/uikit/RippleHelper.java
@@ -0,0 +1,16 @@
+package didikee.github.helper.uikit;
+
+import android.graphics.drawable.Drawable;
+
+public final class RippleHelper {
+
+
+
+
+    private static class Builder{
+        public int rippleColor;
+        public int backgroundColor;
+        public Drawable background;
+        public Drawable mask;
+    }
+}
